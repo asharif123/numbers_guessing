c08801923500f8e7b796a5b13a946c43594f13dd
asharif123 <awadsharif9@gmail.com>
2023-11-11 12:59:38 -0800

    deleted vs folder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿namespace number_guessing
+﻿/*This game involves a secret number being chosen by the computer within a specific range (let’s say 1-100) and the player then tries to guess this number.
+
+If the number they guess is too low, the program will say “too low” and if it is too high it will reply with “too high”. The player wins when they guess the correct number.
+
+Added Difficulty: Put a limit on how many wrong guesses they can have. Too many and the game ends with “You lose”.
+
+rocket Tips: First thing to look at is generating a random number. Despite the language you choose, most of the time a random number can be created using a random generator function or object.
+.NET has the “Random” object and C++ has rand().
+Once you have a random number chosen, ask the player for a guess.
+Compare their guess to this random number chosen. An easy if statement can be used to see if it is too high or too low. If they are equal, tell the player they win and restart the game.
+*/
+
+namespace number_guessing
 {
     internal class Program
     {
