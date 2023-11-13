79800499db6a0045c18dc389710fcaeecafb6f36
asharif123 <awadsharif9@gmail.com>
2023-11-12 21:00:24 -0800

    got rid of unnecessary comments

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             //Generate a random number between 1 to 100
             Random rng = new Random();
