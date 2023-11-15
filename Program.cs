aaf4a2f33757a36c69a9136ca6bcb211fb169976
asharif123 <awadsharif9@gmail.com>
2023-11-14 19:17:29 -0800

    fixed issue #2 by getting rid of magic numbers in console.writeline

--- Program.cs
+++ Program.cs
@@ -15,10 +15,11 @@
             const int NO_MORE_GUESSES_LEFT = 0;
             //max difference between random number and user guess to show if user is close
             const int MAX_DIFFERENCE = 5;
+
             //for loop to ask user to guess a number
             for (int GuessesLeft = MAX_NUM_OF_GUESSES; GuessesLeft >= NO_MORE_GUESSES_LEFT; GuessesLeft--)
             {
-                Console.WriteLine("\nGuess a number between 1 to 100!\n");
+                Console.WriteLine($"\nGuess a number between {MINIMUM_RANDOM_NUMBER} to {MAXIMUM_RANDOM_NUMBER - 1}!\n");
                 int userGuess = int.Parse(Console.ReadLine());
                 //as soon as user enters value, immediately determine if it falls between 1 to 100 range
                 if (userGuess < MINIMUM_RANDOM_NUMBER || userGuess > MAXIMUM_RANDOM_NUMBER - 1)
@@ -31,7 +32,6 @@
                     Console.WriteLine("\nYou guessed correctly!");
                     break;
                 }
-
                 //calculate the absolute value of difference between randomNumber & userGuess
                 int diffBetweenNumbers = Math.Abs(randomNumber - userGuess);
                 //check if user's guess is off by 5
@@ -62,6 +62,7 @@
                     Console.Write($"\nGame Over! The correct number is {randomNumber}!\n");
                 }
             }
+
         }
     }
 }
