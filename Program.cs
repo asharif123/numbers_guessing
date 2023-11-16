024f52e8ef40a1b1d06498c1f24c915a671031d7
asharif123 <awadsharif9@gmail.com>
2023-11-15 21:42:14 -0800

    fixed issue #11 by optimizing if statement checking user's guess

--- Program.cs
+++ Program.cs
@@ -15,9 +15,11 @@
             const int NO_MORE_GUESSES_LEFT = 0;
             //max difference between random number and user guess to show if user is close
             const int MAX_DIFFERENCE = 5;
-
+            //variables to define number of guesses left use to define game over code outside for loop
+            int guessesLeft;
             //for loop to ask user to guess a number
-            for (int GuessesLeft = MAX_NUM_OF_GUESSES; GuessesLeft >= NO_MORE_GUESSES_LEFT; GuessesLeft--)
+
+            for (guessesLeft = MAX_NUM_OF_GUESSES; guessesLeft >= NO_MORE_GUESSES_LEFT; guessesLeft--)
             {
                 Console.WriteLine($"\nGuess a number between {MINIMUM_RANDOM_NUMBER} to {MAXIMUM_RANDOM_NUMBER - 1}!\n");
                 int userGuess = int.Parse(Console.ReadLine());
@@ -38,31 +40,30 @@
                 //calculate the absolute value of difference between randomNumber & userGuess
                 int diffBetweenNumbers = Math.Abs(randomNumber - userGuess);
 
-                //check if user's guess is off by 5
-                //added at beginning to ensure user is aware that guess is off by 5
-                //otherwise user will never know if he's close to guessing the correct number
-                if (userGuess < randomNumber && diffBetweenNumbers <= MAX_DIFFERENCE)
+                //if statement to see if user enters a number less than or greater than randomNumber
+                if (userGuess < randomNumber)
                 {
-                    Console.WriteLine($"\nYour guess is too low but you are close! You have {GuessesLeft} guesses left!");
-                }
-                else if (userGuess < randomNumber)
-                {
-                    Console.WriteLine($"\nYour guess is too low! You have {GuessesLeft} guesses left!");
-                }
-                else if (userGuess > randomNumber && diffBetweenNumbers <= MAX_DIFFERENCE)
-                {
-                    Console.WriteLine($"\nYour guess is too high but you are close! You have {GuessesLeft} guesses left!");
+                    Console.WriteLine("\nGuess is too low!");
                 }
                 else
                 {
-                    Console.WriteLine($"\nYou guess is too high! You have {GuessesLeft} guesses left!");
+                    Console.WriteLine("\nGuess is too high!");
                 }
+
+                //separate if statement to determine if user's guess is no more than 5 off from randomNumber
+                if (diffBetweenNumbers <= MAX_DIFFERENCE)
+                {
+                    Console.WriteLine("\nYou are close!");
+                }
+
+                //append number of guesses that user has left based on above if conditions
+                Console.WriteLine($"\nYou have {guessesLeft} guesses left!");
 
                 //Game ends if user fails to guess the number in 5 attempts.
                 //and statement needed because if user enters correct number on last guess
                 //it will show user was correct and not show user how many guesses are left
                 //add game over statement at end to ensure user sees game over as the last message
-                if (GuessesLeft == NO_MORE_GUESSES_LEFT && userGuess != randomNumber)
+                if (guessesLeft == NO_MORE_GUESSES_LEFT && userGuess != randomNumber)
                 {
                     Console.Write($"\nGame Over! The correct number is {randomNumber}!\n");
                 }
