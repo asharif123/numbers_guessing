77a9732afdef1f5979e901d412e348661b3381ee
asharif123 <awadsharif9@gmail.com>
2023-11-15 22:23:41 -0800

    fixed issue #7 by moving game over code to end of program after for loop

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nYou have 5 chances to guess the correct number!\n");
             //Generate a random number between 1 to 100
             Random rng = new Random();
             //define random number constraints
@@ -11,18 +12,20 @@
             const int MAXIMUM_RANDOM_NUMBER = 101;
             int randomNumber = rng.Next(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
             //number of guesses that user has to guess the correct number
-            const int MAX_NUM_OF_GUESSES = 4;
+            const int MAX_NUM_OF_GUESSES = 5;
             const int NO_MORE_GUESSES_LEFT = 0;
             //max difference between random number and user guess to show if user is close
-            const int MAX_DIFFERENCE = 5;
-            //variables to define number of guesses left use to define game over code outside for loop
+            const int MAX_DIFFERENCE = 4;
+            //variables to define number of guesses left and userGuess use to define game over code outside for loop
+            //needed since guessesLeft and userGuess are used in for loop
             int guessesLeft;
+            int userGuess = 0;
 
             //for loop to ask user to guess a number
-            for (guessesLeft = MAX_NUM_OF_GUESSES; guessesLeft >= NO_MORE_GUESSES_LEFT; guessesLeft--)
+            for (guessesLeft = MAX_NUM_OF_GUESSES; guessesLeft > NO_MORE_GUESSES_LEFT; guessesLeft--)
             {
                 Console.WriteLine($"\nGuess a number between {MINIMUM_RANDOM_NUMBER} to {MAXIMUM_RANDOM_NUMBER - 1}!\n");
-                int userGuess = int.Parse(Console.ReadLine());
+                userGuess = int.Parse(Console.ReadLine());
 
                 //as soon as user enters value, immediately determine if it falls between 1 to 100 range
                 if (userGuess < MINIMUM_RANDOM_NUMBER || userGuess > MAXIMUM_RANDOM_NUMBER - 1)
@@ -57,16 +60,14 @@
                 }
 
                 //append number of guesses that user has left based on above if conditions
-                Console.WriteLine($"\nYou have {guessesLeft} guesses left!");
+                //subtract guessesLeft by 1 to give accurate number of guesses that user has left
+                Console.WriteLine($"\nYou have {guessesLeft - 1} guesses left!");
+            }
 
-                //Game ends if user fails to guess the number in 5 attempts.
-                //and statement needed because if user enters correct number on last guess
-                //it will show user was correct and not show user how many guesses are left
-                //add game over statement at end to ensure user sees game over as the last message
-                if (guessesLeft == NO_MORE_GUESSES_LEFT && userGuess != randomNumber)
-                {
-                    Console.Write($"\nGame Over! The correct number is {randomNumber}!\n");
-                }
+            //Game ends if user fails to guess the number in 5 attempts.
+            if (guessesLeft == NO_MORE_GUESSES_LEFT)
+            {
+                Console.Write($"\nGame Over! The correct number is {randomNumber}!\n");
             }
         }
     }
